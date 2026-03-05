cd98f58b0267302bdeb2b9a7724758f788eed096
Ayo-Ajayi Oluwatokiloba <tokkyayoajayi@gmail.com>
2026-03-05 11:01:22 +0100

    feat: generate list of cafes based on your location

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, HTTPException,Depends
 from models.cafe import Cafe ,SpecificCafe
 from sqlalchemy.orm import Session
-from database.connect_DB import get_db,engine
+from sqlalchemy import func
+from database.connect_DB import get_db
 from models.cafemodel import CafeDB
 app = FastAPI(title= "Cafe finder",description="beginner project to help you find the cafe closet to you")
 
@@ -21,3 +22,27 @@ def return_cafebyid(id:int,db:Session= Depends(get_db)):
     if not cafe:
         raise HTTPException(status_code=404,detail="Cafe Not in Dataset") 
     return cafe
+
+@app.get("/NEAR-ME",response_model=list[SpecificCafe])
+def cafe_near_me(long:float,lat:float,radius:int = 2000,db:Session= Depends(get_db)):
+    #This creates the 'reference pin' inside the query
+    user_location = func.ST_SetSRID(func.ST_MakePoint(long, lat), 4326)
+
+    # 1. Start the query on the Cafe model
+    query = db.query(CafeDB)
+
+    # 2. Apply the Spatial Filter
+    query = query.filter(func.ST_DWithin(
+                            CafeDB.location, 
+                            user_location, 
+                            radius))
+
+    # 3. Sort by Distance (Closest first)
+    query = query.order_by(func.ST_Distance(
+                            CafeDB.location, 
+                            user_location))
+
+    # 4. Execute the query
+    cafes = query.all()
+
+    return cafes
